dcc14fdeadab85541e2ac21c95cde292b363d468
Sanjaya Koju <40861550+sanjayakoju@users.noreply.github.com>
2022-08-05 16:17:34 -0500

    Job Search Application Creating

---
+++ src/main/java/com/ea/cs544/jobsearchapplication/JobSearchApplication.java
@@ -0,0 +1,13 @@
+package com.ea.cs544.jobsearchapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ea/cs544/jobsearchapplication/JobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea.cs544.jobsearchapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
